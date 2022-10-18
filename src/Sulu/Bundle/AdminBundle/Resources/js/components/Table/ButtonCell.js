3215ac20b16fde685651fc98d56b95555b777034
Manfred Wuits <manfred@mavu.io>
2022-10-18 12:03:45 +0200

    added real a-links in ButtonCell

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Table/ButtonCell.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Table/ButtonCell.js
@@ -17,7 +17,12 @@ export default class ButtonCell extends React.PureComponent<Props> {
         disabled: false,
     };
 
-    handleClick = () => {
+
+    // mavu added real a-tags here: 2022-10-18
+
+    handleClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+        event.preventDefault(); // do not follow href by default
+
         const {rowIndex, onClick, rowId} = this.props;
 
         if (onClick) {
@@ -29,8 +34,19 @@ export default class ButtonCell extends React.PureComponent<Props> {
         const {
             disabled,
             icon,
+            rowId,
         } = this.props;
 
+        if(typeof rowId === 'string'  && icon=='su-pen' && !disabled){
+        return (
+            <Cell className={tableStyles.buttonCell}>
+                <a  href={`/admin/#/webspaces/mainsite/pages/de/${rowId}/details`}
+                    onClick={this.handleClick}>
+                    <Icon name={icon} />
+                </a>
+            </Cell>
+        );
+        } else {
         return (
             <Cell className={tableStyles.buttonCell}>
                 <button disabled={disabled} onClick={this.handleClick} type="button">
@@ -38,5 +54,6 @@ export default class ButtonCell extends React.PureComponent<Props> {
                 </button>
             </Cell>
         );
+        }
     }
 }
