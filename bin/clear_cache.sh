af53ce1df8dfa7647e738d59ec1d9a97a43bea9b
Manfred Wuits <manfred@mavu.io>
2023-08-21 21:39:32 +0200

    fixed clear_cache scripts,  added custom tarsana/functional

--- bin/clear_cache.sh
+++ bin/clear_cache.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-sudo runuser -u www-data -- php bin/websiteconsole cache:clear  --env=prod
-sudo runuser -u www-data -- php bin/adminconsole cache:clear  --env=prod
-sudo runuser -u www-data -- php bin/adminconsole fos:httpcache:clear  --env=prod
+#!/usr/bin/bash
+php bin/websiteconsole cache:clear  --env=$APP_ENV
+php bin/adminconsole cache:clear  --env=$APP_ENV
+php bin/adminconsole fos:httpcache:clear  --env=$APP_ENV
