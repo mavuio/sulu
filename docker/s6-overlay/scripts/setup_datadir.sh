dba90d1a17340df5ffca1cbc2e58c8724e8055a2
Manfred Wuits <manfred@mavu.io>
2023-08-22 23:35:40 +0200

    fixed datadir setup

--- docker/s6-overlay/scripts/setup_datadir.sh
+++ docker/s6-overlay/scripts/setup_datadir.sh
@@ -8,6 +8,13 @@ echo "SETUP DATA-DIR"
 cd /var/www/html
 mkdir -p data/var
 mkdir -p data/public/uploads
+if [ ! -L public/uploads ]; then
+    ln -s ../data/public/uploads public/uploads
+fi
 mkdir -p data/public/fe_assets2
+if [ ! -L public/fe_assets2 ]; then
+    ln -s ../data/public/fe_assets2 public/fe_assets2
+fi
+
 chown -R webuser:webgroup data
 exit 0
